2a4e8689d198c4b3cc097472d10bd461cfd5e6f6
miku <empty>
2025-10-16 00:21:34 +0800

    feat: config get

--- pkg/config/config_parse.go
+++ pkg/config/config_parse.go
@@ -9,17 +9,22 @@ import (
 
 func LoadConfig() *Type {
 	env := os.Getenv("Env")
-	if env == "" {
-		panic("env variable not set")
-	}
-	reader, err := os.ReadFile(fmt.Sprintf("./config/%s.yaml", env))
-	if err != nil {
-		panic(err)
-	}
+	configInject := os.Getenv("ConfigInject")
+	injectMode := configInject == "true"
 	var config *Type
-	err = yaml.Unmarshal(reader, &config)
-	if err != nil {
-		panic(err)
+	if !injectMode {
+		if env == "" {
+			panic("env variable not set")
+		}
+		reader, err := os.ReadFile(fmt.Sprintf("./config/%s.yaml", env))
+		if err != nil {
+			panic(err)
+		}
+		err = yaml.Unmarshal(reader, &config)
+		if err != nil {
+			panic(err)
+		}
+		return config
 	}
 	return config
 }
--- pkg/config/config_type.go
+++ pkg/config/config_type.go
@@ -1,17 +1,31 @@
 package config
 
-type Record map[string]any
+import "os"
+
+type Record map[string]string
 
 type Type struct {
-	AppName     string `yaml:"app_name"`
-	AppPort     int    `yaml:"app_port"`
-	HealthCheck string `yaml:"health_check"` // 健康检查地址
+	AppName     string `yaml:"APP_NAME"`
+	AppPort     int    `yaml:"APP_PORT"`
+	HealthCheck string `yaml:"HEALTH_CHECK"` // 健康检查地址
 
-	Mysql       bool   `yaml:"mysql"`
-	MysqlDsn    string `yaml:"mysql_dsn"`
-	AutoMigrate bool   `yaml:"auto_migrate"`
+	Mysql       bool   `yaml:"MYSQL"`
+	MysqlDsn    string `yaml:"MYSQL_DSN"`
+	AutoMigrate bool   `yaml:"AUTO_MIGRATE"`
 
-	Mongo    bool   `yaml:"mongo"`
-	MongoDsn string `yaml:"mongo_dsn"`
+	Mongo    bool   `yaml:"MONGO"`
+	MongoDsn string `yaml:"MONGO_DSN"`
 	Record
+}
+
+func (t *Type) Get(record string, defaultValue string) string {
+	envValue := os.Getenv(record)
+	if envValue != "" {
+		return envValue
+	}
+	configValue := t.Record[record]
+	if configValue != "" {
+		return configValue
+	}
+	return defaultValue
 }
